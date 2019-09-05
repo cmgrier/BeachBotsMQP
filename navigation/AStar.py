df7bb22984fb38beee68685d60c5d7942e508157
cmgrier <cmgrier@wpi.edu>
2019-09-05 15:54:38 -0400

    optomized, tested, and ready to be merged

--- navigation/AStar.py
+++ navigation/AStar.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python
+import math
 from _heapq import *
-from math import hypot
+
+from support import Constants
 
 
 class AStar:
 
     # given map is an array of weights
     def __init__(self, map):
-        self.map = map
-        self.frontier = list()
-        self.investigated = list()
+        self.__map = map
+        self.__frontier = list()
+        self.__investigated = list()
 
     # start and end are indexes on the map
     # map indexes work as follows:
@@ -22,59 +25,85 @@ class AStar:
 
     # method to be called outside of AStar class to retrieve path
     def find_path(self, start, end):
-        self.frontier = list()
-        node = self.create_node(start, end, 0)
-        heappush(self.frontier, [node, node.cost])
-        while not self.frontier:
-            best_node = heappop(self.frontier)
+        self.__frontier = list()
+        node = Node()
+        node.index = start
+        node.heuristic = self.__get_heuristic(start, end)
+        node.is_start = True
+        heappush(self.__frontier, [node, node.cost])
+
+        while self.__frontier.__len__() > 0:
+            best_node = heappop(self.__frontier)[0]
             if best_node.index == end:
-                return self.create_path(start, best_node)
-            self.investigate_point(best_node, end)
-            self.investigated.append(best_node)
+                return self.__create_path(start, best_node)
+            self.__investigate_point(best_node, end)
+            self.__investigated.append(best_node)
         return start
 
     # checks local points above, below, and to either side of given node
     # and adds them to the frontier
-    def investigate_point(self, node, end):
-        left_node = self.create_node(node.index - 1, end, node.movement_cost + 1)
-        right_node = self.create_node(node.index + 1, end, node.movement_cost + 1)
-        top_node = self.create_node(node.index - self.map.width, end, node.movement_cost + 1)
-        bottom_node = self.create_node(node.index + self.map.width, end, node.movement_cost + 1)
-        new_nodes = [left_node, right_node, top_node, bottom_node]
+    def __investigate_point(self, node, end):
+        new_nodes = list()
+        if node.index - 1 >= 0 and node.index % self.__map.width != 0:
+            left_node = self.__create_node(node.index - 1, end, node.movement_cost + Constants.standard_move_cost, node)
+            new_nodes.append(left_node)
+
+        if node.index + 1 < self.__map.width * self.__map.width and node.index % self.__map.width != 3:
+            right_node = self.__create_node(node.index + 1, end, node.movement_cost + Constants.standard_move_cost, node)
+            new_nodes.append(right_node)
+
+        if node.index - self.__map.width > 0:
+            top_node = self.__create_node(node.index - self.__map.width, end, node.movement_cost + Constants.standard_move_cost, node)
+            new_nodes.append(top_node)
+
+        if node.index + self.__map.width < self.__map.width * self.__map.width:
+            bottom_node = self.__create_node(node.index + self.__map.width, end, node.movement_cost + Constants.standard_move_cost, node)
+            new_nodes.append(bottom_node)
+
         for new_node in new_nodes:
-            if self.is_node_in_frontier(new_node) and not self.is_node_investigated(new_node):
-                self.update_node(new_node)
-            else:
-                heappush(self.frontier, [new_node, new_node.cost])
+            if self.__is_node_in_frontier(new_node) and not self.__is_node_investigated(new_node):
+                self.__update_node(new_node)
+            elif not self.__is_node_investigated(new_node):
+                heappush(self.__frontier, [new_node, new_node.cost])
 
     # checks if the node is already in frontier
-    def is_node_in_frontier(self, node):
-        for frontier_node in self.frontier:
+    def __is_node_in_frontier(self, node):
+        for frontier_node in self.__frontier:
             if frontier_node.index == node.index:
                 return True
         return False
 
     # checks if the node has already been investigated
-    def is_node_investigated(self, node):
-        for investigated_node in self.investigated:
+    def __is_node_investigated(self, node):
+        for investigated_node in self.__investigated:
             if investigated_node.index == node.index:
                 return True
         return False
 
     # Updates the node if it already exists in the frontier and has a shorter path
-    def update_node(self, new_node):
-        for node in self.frontier:
-            self.update_node_cost(new_node, node)
+    def __update_node(self, new_node):
+        for node in self.__frontier:
+            self.__update_node_cost(new_node, node)
+
+    def __get_direction(self, node):
+        # 1 is horizontal direction, 0 is vertical
+        if node.index - node.previous_point.index > abs(1):
+            return 1
+        else:
+            return 0
+
+    def __is_turning(self, node):
+        return self.__get_direction(node) != self.__get_direction(node.previous_point)
 
     # helper for self.update_node_in_frontier
-    def update_node_cost(self, new_node, old_node):
+    def __update_node_cost(self, new_node, old_node):
         if old_node.index == new_node.index and new_node.cost < old_node.cost:
-            self.frontier.remove([old_node, old_node.cost])
-            heapify(self.frontier)
-            heappush(self.frontier, [new_node, new_node.cost])
+            self.__frontier.remove([old_node, old_node.cost])
+            heapify(self.__frontier)
+            heappush(self.__frontier, [new_node, new_node.cost])
 
     # this creates path from the given start and end node
-    def create_path(self, start_index, end):
+    def __create_path(self, start_index, end):
         path = list()
         path.append(end.index)
         current_node = end.previous_point
@@ -84,27 +113,28 @@ class AStar:
         return path
 
     # creates a new node object from the given index, end index, and current movement cost
-    def create_node(self, position, end, movement_cost):
-        node = Node
-        node.heuristic = self.get_heuristic(position, end)
-        node.movement_cost = movement_cost
+    def __create_node(self, position, end, movement_cost, origin):
+        node = Node()
+        node.heuristic = self.__get_heuristic(position, end)
+        node.movement_cost = movement_cost + self.__map.map[position] * Constants.difficult_terrain_factor
         node.index = position
         node.cost = node.heuristic + node.movement_cost
+        node.previous_point = origin
+        if not node.previous_point.is_start and self.__is_turning(node):
+            node.movement_cost += Constants.turning_factor
         return node
 
     # returns the estimated cost between the two given points
-    def get_heuristic(self, start, end):
-        start_point = self.convert_index_to_point(start)
-        end_point = self.convert_index_to_point(end)
-        return hypot(start_point.x - end_point.x, start_point.y - end_point.y)
+    def __get_heuristic(self, start, end):
+        start_point = self.__convert_index_to_point(start)
+        end_point = self.__convert_index_to_point(end)
+        return math.sqrt(math.pow(start_point[0] - end_point[0], 2) + math.pow(start_point[1] - end_point[1], 2))
 
     # converts the given index to an xy point. Used to find heuristic
-    def convert_index_to_point(self, index):
-        x = index % self.map.width
-        y = index / self.map.width
-        point = Point
-        point.x = x
-        point.y = y
+    def __convert_index_to_point(self, index):
+        x = int(index % self.__map.width)
+        y = int(index / self.__map.width)
+        point = [x, y]
         return point
 
 
@@ -114,11 +144,28 @@ class Node:
     heuristic = 0
     movement_cost = 0
     index = 0
-    previous_point = 0
+    previous_point = None
+    is_start = False
+
+    def __lt__(self, other):
+        if isinstance(other, self.__class__):
+            return self.cost < other.cost
+        return NotImplemented
 
 
-# delete later once ROS is included in workspace
-# will be replaced by ROS.Point data
-class Point:
-    x = 0
-    y = 0
+class Map:
+    map = list()
+    width = 0
+
+
+# for testing
+if __name__ == '__main__':
+    map = Map()
+    map.map = [1, 1, 1, 1,
+               1, 1, 1, 10,
+               1, 1, 1, 1,
+               1, 1, 10, 1]  # length = 16
+    map.width = int(math.sqrt(map.map.__len__()))
+    a_star = AStar(map)
+    path = a_star.find_path(0, 15)
+    print(path)
