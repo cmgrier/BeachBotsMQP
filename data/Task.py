aee03949fe9a692e58b2ff6a59cfbe4e3db2decf
Your Name <riverstone98@gmail.com>
2019-09-09 18:23:10 -0400

    update task

--- data/Task.py
+++ data/Task.py
@@ -1,7 +1,19 @@
 # data of the different tasks to be completed by the small bots
 class Task:
-    def __init__(self, zone):
+    def __init__(self, zone, type=None):
         self.isActive = False
         self.isComplete = False
         self.workerID = -1
+        self.type = type or "clean"
         self.zone = zone
+        self.priority = 1
+        self.set_priority()
+
+#set the priority level of the task based on the type
+    def set_priority(self):
+        if self.type is "clean":
+            self.priority = 3
+        elif self.type is "avoid":
+            self.priority = 1
+        elif self.type is "dump":
+            self.priority = 2
--- support/Constants.py
+++ support/Constants.py
@@ -1,2 +1,3 @@
 storedMapSize = 10
 safeDistanceBetweenBots = 100
+trashBinThreshold = 12    # TODO: Get the actual value for this constant
