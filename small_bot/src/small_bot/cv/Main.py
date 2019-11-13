b2b5f6c6e24b5e608d039ac251482e20e2cdd8cb
Everett Johnson <erjohnson@wpi.edu>
2019-11-12 19:15:56 -0500

    Added Computer Vision Code folder and minor code

---
+++ small_bot/src/small_bot/cv/Main.py
@@ -0,0 +1,28 @@
+# This will be the main for the Computer Vision
+
+import numpy as np
+import cv2
+
+
+class Main():
+
+    def __init__(self):
+
+        self.cap = cv2.VideoCapture(0)
+
+    def test_cam(self):
+        while True:
+            # Capture frame-by-frame
+            ret, frame = self.cap.read()
+
+            # Our operations on the frame come here
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Display the resulting frame
+            cv2.imshow('frame', gray)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+if __name__ == "__main__":
+    m = Main()
