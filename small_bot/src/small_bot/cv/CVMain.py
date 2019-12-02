a44cf4581f7102ccaf91349cfa4f57a02059001a
Everett Johnson <erjohnson@wpi.edu>
2019-12-02 14:16:35 -0500

    More vision work

--- small_bot/src/small_bot/cv/CVMain.py
+++ small_bot/src/small_bot/cv/CVMain.py
@@ -152,7 +152,7 @@ class CVMain:
 
         # Get filters from a small left corner
         left_low_filter, left_high_filter = self.small_segment_filter_generator(frame, l_y1, l_y2, l_x1, l_x2,
-                                                                                expansion=10)
+                                                                                expansion=5)
 
         print(left_low_filter, left_high_filter)
 
