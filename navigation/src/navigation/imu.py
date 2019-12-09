cfef7be08a8a3f261c9b3a33fd37a816cf1f89c2
Your Name <riverstone98@gmail.com>
2019-12-09 16:00:28 -0500

    calivrate fix

--- navigation/src/navigation/imu.py
+++ navigation/src/navigation/imu.py
@@ -49,7 +49,7 @@ class IMU:
         return math.degrees(radians)
 
     def calibrate(self):
-        gyroZAvg = 0
+        gyroZAvg = 0.0
         for x in range(1,5):
             self.bus = smbus.SMBus(1)  # bus = smbus.SMBus(0) fuer Revision 1
             self.address = 0x68  # via i2cdetect
@@ -57,7 +57,7 @@ class IMU:
             self.bus.write_byte_data(self.address, power_mgmt_1, 0)
             gyroskop_zout = self.read_word_2c(0x47)
             gyroZAvg += gyroskop_zout/131
-        self.gyroZ = gyroZAvg/5
+        self.gyroZ = gyroZAvg/5.0
 
     def pub_imu(self):
 
