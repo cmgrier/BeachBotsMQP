36dacaa66702c6b3dbb89474e2516937d8f1b91f
Your Name <riverstone98@gmail.com>
2020-02-03 19:52:10 -0500

    slight edit

--- small_bot/src/small_bot/collector_arm/ArmController.py
+++ small_bot/src/small_bot/collector_arm/ArmController.py
@@ -1,9 +1,10 @@
-from small_bot.Kinematics import Kinematics
+#!/usr/bin/python
+from small_bot.collector_arm.Kinematics import Kinematics
 
 
 class ArmController:
 
-    def __init__(self,pin0,pin1,pin2):
+    def __init__(self, pin0, pin1, pin2):
         """
         Constructor for ArmController class
         :param pin0: pin for joint 0 stepper motor
@@ -14,34 +15,42 @@ class ArmController:
         self.pin1 = pin1
         self.pin2 = pin2
         self.kin = Kinematics()
+        self.calibrate_joints()
 
-    def move_end_effector(self,x,y):
+    def move_end_effector(self, x, y):
         """
         Move the end effector to the xy-coordinates
         :param x: final x coordinate with respect to the end effector in meters
         :param y: final y coordinate with respect to the end effector in meters
-        :return:
+        :return: True if executed properly
         """
         #TODO
-        angles = self.kin.invkin(x,y)
-        self.turn_joint0(angles[0])
-        self.turn_joint1(angles[1])
+        try:
+            angles = self.kin.invkin(x,y)
+            self.turn_joint0(angles[0])
+            self.turn_joint1(angles[1])
+            return True
+        except ValueError:
+            error = 'The location ('+str(x)+','+str(y)+') is out of the task space'
+            print(error)
+            return False
 
-
-    def turn_joint0(self,angle):
+    def turn_joint0(self, angle):
         """
         Turn the stepper motor for joint 0 to a specific angle
         :param angle: angle of the joint to turn to in degrees
         :return:
         """
+        print("joimt0: ",angle)
         #TODO
 
-    def turn_joint1(self,angle):
+    def turn_joint1(self, angle):
         """
         Turn the servo for joint 1 to a specific angle
         :param angle: angle of the joint to turn to in degrees
         :return:
         """
+        print("joimt1: ",angle)
         #TODO
 
     def calibrate_joints(self):
@@ -51,10 +60,15 @@ class ArmController:
         """
         #TODO
 
-    def close_gripper(self,status):
+    def close_gripper(self, status):
         """
         Moves the servo for the gripper
         :param status: True closes the gripper and False opens it
         :return:
         """
         #TODO
+
+
+if __name__=="__main__":
+    arm = ArmController(1,2,3)
+    arm.move_end_effector(40,0)
