c835b1e0f2d2ff88b048f161b3968fb2ecf5f2f0
Everett Johnson <erjohnson@wpi.edu>
2020-03-20 14:02:07 -0400

    Another Attempt

--- small_bot/src/small_bot/cv/coral_test.py
+++ small_bot/src/small_bot/cv/coral_test.py
@@ -1,143 +1,75 @@
-# based on https://github.com/tensorflow/examples/blob/master/lite/examples/object_detection/raspberry_pi/detect_picamera.py
-from tflite_runtime.interpreter import Interpreter, load_delegate
+# import the necessary packages
+from edgetpu.detection.engine import DetectionEngine
+from imutils.video import VideoStream
+from PIL import Image
 import argparse
+import imutils
 import time
 import cv2
-import re
-from PIL import Image, ImageDraw, ImageFont
-import numpy as np
 
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--model", required=True,
+                help="path to TensorFlow Lite object detection model")
+ap.add_argument("-l", "--labels", required=True,
+                help="path to labels file")
+ap.add_argument("-c", "--confidence", type=float, default=0.3,
+                help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
 
-def draw_image(image, results, labels, size):
-    result_size = len(results)
-    for idx, obj in enumerate(results):
-        print(obj)
-        # Prepare image for drawing
-        draw = ImageDraw.Draw(image)
+# initialize the labels dictionary
+print("[INFO] parsing class labels...")
+labels = {}
+# loop over the class labels file
+for row in open(args["labels"]):
+    # unpack the row and update the labels dictionary
+    (classID, label) = row.strip().split(maxsplit=1)
+    labels[int(classID)] = label.strip()
+# load the Google Coral object detection model
+print("[INFO] loading Coral model...")
+model = DetectionEngine(args["model"])
+# initialize the video stream and allow the camera sensor to warmup
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+# vs = VideoStream(usePiCamera=False).start()
+time.sleep(2.0)
 
-        # Prepare boundary box
-        ymin, xmin, ymax, xmax = obj['bounding_box']
-        xmin = int(xmin * size[0])
-        xmax = int(xmax * size[0])
-        ymin = int(ymin * size[1])
-        ymax = int(ymax * size[1])
-
-        # Draw rectangle to desired thickness
-        for x in range( 0, 4 ):
-            draw.rectangle((ymin, xmin, ymax, xmax), outline=(255, 255, 0))
-
-        # Annotate image with label and confidence score
-        display_str = labels[obj['class_id']] + ": " + str(round(obj['score']*100, 2)) + "%"
-        draw.text((box[0], box[1]), display_str, font=ImageFont.truetype("/usr/share/fonts/truetype/piboto/Piboto-Regular.ttf", 20))
-
-        displayImage = np.asarray( image )
-        cv2.imshow('Coral Live Object Detection', displayImage)
-
-
-def load_labels(path):
-    """Loads the labels file. Supports files with or without index numbers."""
-    with open(path, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-        labels = {}
-        for row_number, content in enumerate(lines):
-            pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
-            if len(pair) == 2 and pair[0].strip().isdigit():
-                labels[int(pair[0])] = pair[1].strip()
-            else:
-                labels[row_number] = pair[0].strip()
-    return labels
-
-
-def set_input_tensor(interpreter, image):
-    """Sets the input tensor."""
-    tensor_index = interpreter.get_input_details()[0]['index']
-    input_tensor = interpreter.tensor(tensor_index)()[0]
-    input_tensor[:, :] = image
-
-
-def get_output_tensor(interpreter, index):
-    """Returns the output tensor at the given index."""
-    output_details = interpreter.get_output_details()[index]
-    tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
-    return tensor
-
-
-def detect_objects(interpreter, image, threshold):
-    """Returns a list of detection results, each a dictionary of object info."""
-    set_input_tensor(interpreter, image)
-    interpreter.invoke()
-
-    # Get all output details
-    boxes = get_output_tensor(interpreter, 0)
-    classes = get_output_tensor(interpreter, 1)
-    scores = get_output_tensor(interpreter, 2)
-    count = int(get_output_tensor(interpreter, 3))
-
-    results = []
-    for i in range(count):
-        if scores[i] >= threshold:
-            result = {
-                'bounding_box': boxes[i],
-                'class_id': classes[i],
-                'score': scores[i]
-            }
-            results.append(result)
-    return results
-
-
-def make_interpreter(model_file, use_edgetpu):
-    model_file, *device = model_file.split('@')
-    if use_edgetpu:
-        return Interpreter(
-            model_path=model_file,
-            experimental_delegates=[
-                load_delegate('libedgetpu.so.1',
-                {'device': device[0]} if device else {})
-            ]
-        )
-    else:
-        return Interpreter(model_path=model_file)
-
-
-def main():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-m', '--model', type=str, required=True, help='File path of .tflite file.')
-    parser.add_argument('-l', '--labels', type=str, required=True, help='File path of labels file.')
-    parser.add_argument('-t', '--threshold', type=float, default=0.4, required=False, help='Score threshold for detected objects.')
-    parser.add_argument('-v', '--video', type=str, required=True, help='Path to video')
-    parser.add_argument('-e', '--use_edgetpu', action='store_true', default=False, help='Use EdgeTPU')
-    args = parser.parse_args()
-
-    labels = load_labels(args.labels)
-    interpreter = make_interpreter(args.model, args.use_edgetpu)
-    interpreter.allocate_tensors()
-    _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
-
-    # Initialize video stream
-    video = cv2.VideoCapture(video)
-    time.sleep(1)
-
-    while(video.isOpened()):
-        try:
-            ret, frame = video.read()
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame_resized = cv2.resize(frame_rgb, (input_width, input_height))
-            input_data = np.expand_dims(frame_resized, axis=0)
-            # Perform inference
-            results = detect_objects(interpreter, input_data, args.threshold)
-
-            draw_image(image, results, labels, image.size)
-
-            if( cv2.waitKey( 5 ) & 0xFF == ord( 'q' ) ):
-                fps.stop()
-                break
-        except KeyboardInterrupt:
-            break
-
-    cv2.destroyAllWindows()
-    video.release()
-    time.sleep(2)
-
-
-if __name__ == '__main__':
-    main()
+# loop over the frames from the video stream
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 500 pixelsl
+    frame = vs.read()
+    frame = imutils.resize(frame, width=500)
+    orig = frame.copy()
+    # prepare the frame for object detection by converting (1) it
+    # from BGR to RGB channel ordering and then (2) from a NumPy
+    # array to PIL image format
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = Image.fromarray(frame)
+    # make predictions on the input frame
+    start = time.time()
+    results = model.DetectWithImage(frame, threshold=args["confidence"],
+                                    keep_aspect_ratio=True, relative_coord=False)
+    end = time.time()
+    # loop over the results
+    for r in results:
+        # extract the bounding box and box and predicted class label
+        box = r.bounding_box.flatten().astype("int")
+        (startX, startY, endX, endY) = box
+        label = labels[r.label_id]
+        # draw the bounding box and label on the image
+        cv2.rectangle(orig, (startX, startY), (endX, endY),
+                      (0, 255, 0), 2)
+        y = startY - 15 if startY - 15 > 15 else startY + 15
+        text = "{}: {:.2f}%".format(label, r.score * 100)
+        cv2.putText(orig, text, (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    # show the output frame and wait for a key press
+    cv2.imshow("Frame", orig)
+    key = cv2.waitKey(1) & 0xFF
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
