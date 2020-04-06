07e5dc3f69da0d398b5942a615bbf7727a365a44
srtidd176 <riverstone98@gmail.com>
2020-04-05 21:32:29 -0400

    test updated

--- test/src/test/arm_test.py
+++ test/src/test/arm_test.py
@@ -7,23 +7,31 @@
 # notes		:
 # python_version :3.5
 # =============================================================================
-#from small_bot.collector_arm.ArmController import ArmController
+import sys
+sys.path.insert(1,'/catkin_ws/src/BeachCleanersMQP/small_bot/src/small_bot/collector_arm/ArmController')
+import ArmController
 import RPi.GPIO as GPIO
 import rospy
 from support.Constants import *
 
-def init():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(SWITCH,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	
+class ArmTest:
+	def __init__(self):
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(SWITCH,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		self.arm = ArmController()
+
+	def test_switch(self):
+	     if GPIO.input(SWITCH):
+                 print("3.3V")
+             else:
+                 print("0.0V")
+		
 
 if __name__ == "__main__":
 	try:
-	   init()
-           while True:
-	      if GPIO.input(SWITCH):
-	         print("3.3V")
-	      else:
-		 print("0.0V")
+	   armTest = ArmTest()
+           #while True:
+	    #  armTest.test_switch()
 	except KeyboardInterrupt:
 	  GPIO.cleanup()	
+     
