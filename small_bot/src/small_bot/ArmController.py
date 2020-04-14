9679aa27439db2ec8f9dfcd7b341d4f5192f315d
Everett Johnson <erjohnson@wpi.edu>
2020-04-13 20:47:12 -0400

    Working on fixing gripper

--- small_bot/src/small_bot/ArmController.py
+++ small_bot/src/small_bot/ArmController.py
@@ -9,6 +9,7 @@
 # ==============================================================================
 from small_bot.Kinematics import Kinematics
 import RPi.GPIO as GPIO
+import pigpio
 import rospy
 from support.Constants import *
 
@@ -19,18 +20,24 @@ class ArmController:
         """
         Constructor for ArmController class
         """
+
+        self.pi = pigpio.pi()  # Initialize Pi
+
         self.kin = Kinematics()
         self.joint0_current = 0.0
         self.joint1_current = 0.0
         self.delay = 0.001
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(SM_DIRECTION, GPIO.OUT)
         GPIO.setup(SM_STEP, GPIO.OUT)
         GPIO.setup(JOINT1_SERVO, GPIO.OUT)
-        GPIO.setup(GRIPPER_SERVO, GPIO.OUT)
+        self.gripper_servo_pin = GRIPPER_SERVO
+        # GPIO.setup(GRIPPER_SERVO, GPIO.OUT)
         self.joint1_pwm = GPIO.PWM(JOINT1_SERVO, 50)
-        self.gripper_pwm = GPIO.PWM(GRIPPER_SERVO, 50)
+
+        # self.gripper_pwm = GPIO.PWM(GRIPPER_SERVO, 50)
         self.calibrate_joints()
 
     def move_end_effector(self, x, y):
@@ -42,16 +49,15 @@ class ArmController:
         """
 
         try:
-            angles = self.kin.invkin(x,y)
+            angles = self.kin.invkin(x, y)
             self.turn_joint0(angles[0])
             self.turn_joint1(angles[1])
             return True
         except ValueError:
-            error = 'The location ('+str(x)+','+str(y)+') is out of the task space'
+            error = 'The location (' + str(x) + ',' + str(y) + ') is out of the task space'
             print(error)
             return False
 
-    
     def turn_joint0(self, angle):
         """
         Turn the stepper motor for joint 0 to a specific angle
@@ -59,20 +65,21 @@ class ArmController:
         :return:
         """
         target = angle - self.joint0_current
-        steps = abs(int(target//STEP_ANGLE))
+        steps = abs(int(target // STEP_ANGLE))
         num_steps = 0
-	print("target: ",target)
-	print("steps: ",steps)
+        print("target: ", target)
+        print("steps: ", steps)
+
         if target < 0:
-            GPIO.output(SM_DIRECTION,GPIO.HIGH)
+            GPIO.output(SM_DIRECTION, GPIO.HIGH)
         else:
-            GPIO.output(SM_DIRECTION,GPIO.LOW)
+            GPIO.output(SM_DIRECTION, GPIO.LOW)
         while num_steps < steps:
-            GPIO.output(SM_STEP,GPIO.HIGH)
-	    rospy.sleep(self.delay)
-            GPIO.output(SM_STEP,GPIO.LOW)
+            GPIO.output(SM_STEP, GPIO.HIGH)
             rospy.sleep(self.delay)
-	    num_steps += 1
+            GPIO.output(SM_STEP, GPIO.LOW)
+            rospy.sleep(self.delay)
+            num_steps += 1
 
     def turn_joint1(self, angle):
         """
@@ -80,14 +87,14 @@ class ArmController:
         :param angle: angle of the joint to turn to in degrees
         :return:
         """
-        print("joimt1: ",angle)
-        duty =   (angle * 0.035) + JOINT1_START
-	if duty < 0:
-	 duty = 0
+        print("joimt1: ", angle)
+        duty = (angle * 0.035) + JOINT1_START
+        if duty < 0:
+            duty = 0
         self.joint1_pwm.ChangeDutyCycle(duty)
         rospy.sleep(.6)
         self.joint1_pwm.ChangeDutyCycle(0)
-	rospy.sleep(.6)
+        rospy.sleep(.6)
 
     def calibrate_joints(self):
         """
@@ -95,35 +102,33 @@ class ArmController:
         :return:
         """
         trigger = True
-	trigger2 = True
-        #Open gripper
-        self.gripper_pwm.start(GRIPPER_OPEN)
-        #Zero joint1
+        trigger2 = True
+        # Open gripper
+        # self.gripper_pwm.start(GRIPPER_OPEN)
+        # Zero joint1
         self.joint1_pwm.start(JOINT1_START)
 
-        #Zero joint0
-	#Move arm until it triggers the switch
-	GPIO.output(SM_DIRECTION,GPIO.LOW)
-        while trigger:				
-	   GPIO.output(SM_STEP,GPIO.HIGH)
-           rospy.sleep(self.delay)
-           GPIO.output(SM_STEP,GPIO.LOW)
-           rospy.sleep(self.delay)
-	   if GPIO.input(SWITCH):
-		trigger = False
-		break
-	#Move arm off of switch until it deactivates
-	GPIO.output(SM_DIRECTION,GPIO.HIGH)
+        # Zero joint0
+        # Move arm until it triggers the switch
+        GPIO.output(SM_DIRECTION, GPIO.LOW)
+        while trigger:
+            GPIO.output(SM_STEP, GPIO.HIGH)
+            rospy.sleep(self.delay)
+            GPIO.output(SM_STEP, GPIO.LOW)
+            rospy.sleep(self.delay)
+            if GPIO.input(SWITCH):
+                trigger = False
+                break
+        # Move arm off of switch until it deactivates
+        GPIO.output(SM_DIRECTION, GPIO.HIGH)
         while trigger2:
-           GPIO.output(SM_STEP,GPIO.HIGH)
-           rospy.sleep(self.delay)
-           GPIO.output(SM_STEP,GPIO.LOW)
-           rospy.sleep(self.delay)
-           if not GPIO.input(SWITCH):
+            GPIO.output(SM_STEP, GPIO.HIGH)
+            rospy.sleep(self.delay)
+            GPIO.output(SM_STEP, GPIO.LOW)
+            rospy.sleep(self.delay)
+            if not GPIO.input(SWITCH):
                 trigger2 = False
                 break
-	
-	
 
     def move_gripper(self, status):
         """
@@ -131,15 +136,24 @@ class ArmController:
         :param status: True opens the gripper and False closes it
         :return:
         """
-        if status == False:
-            self.gripper_pwm.ChangeDutyCycle(GRIPPER_CLOSE)
-            rospy.sleep(.6)
-            self.gripper_pwm.ChangeDutyCycle(0)
-	    rospy.sleep(.6)
+
+        if not status:
+            self.pi.set_servo_pulsewidth(self.gripper_servo_pin, 1000)
+            rospy.sleep(0.5)
+
+            # self.gripper_pwm.ChangeDutyCycle(GRIPPER_CLOSE)
+            # rospy.sleep(.6)
+            # self.gripper_pwm.ChangeDutyCycle(0)
+            # rospy.sleep(.6)
         else:
-            self.gripper_pwm.ChangeDutyCycle(GRIPPER_OPEN)
-            rospy.sleep(.6)
-            self.gripper_pwm.ChangeDutyCycle(0)
-	    rospy.sleep(.6)
+            self.pi.set_servo_pulsewidth(self.gripper_servo_pin, 1000)
+            rospy.sleep(0.5)
+            # self.gripper_pwm.ChangeDutyCycle(GRIPPER_OPEN)
+            # rospy.sleep(.6)
+            # self.gripper_pwm.ChangeDutyCycle(0)
+            # rospy.sleep(.6)
 
 
+if __name__ == "__main__":
+    ac = ArmController()
+    ac.move_gripper(False)
