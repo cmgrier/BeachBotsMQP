88127eebb5bedac9a35eb25ac1b60eaf8fe94aac
Everett Johnson <everettreecejohnson564@gmail.com>
2020-04-20 17:06:25 -0400

    Test Code

---
+++ test/src/test/servo_controller_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import maestro
+
+
+class ServoController:
+    def __init__(self):
+        servo = maestro.Controller()
+        servo.setAccel(0, 4)  # set servo 0 acceleration to 4
+        servo.setSpeed(1, 10)  # set speed of servo 1
+        servo.setTarget(0, 6000)  # set servo to move to center position
+        # x = servo.getPosition(1)  # get the current position of servo 1
+        servo.close()
+
+
+if __name__ == "__main__":
+    sc = ServoController()
