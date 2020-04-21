f7c1c5e9e04e21b09dc5229678995050fffa19a7
Everett Johnson <everettreecejohnson564@gmail.com>
2020-04-21 18:07:52 -0400

    Made Servo Controller

---
+++ small_bot/src/small_bot/ServoController.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+import maestro
+import rospy
+from std_msgs.msg import Bool
+from support.Constants import *
+
+
+class ServoController:
+    def __init__(self):
+        # Initialization of the node
+        rospy.init_node('ServoController')
+
+        self.servo = maestro.Controller()
+
+        # Pins
+        self.gripper_pin = 0
+        self.elbow_pin = 1
+
+        # ROS Subscribers and Publishers
+        rospy.Subscriber('pickup_flag', Bool, self.pickup_can)
+        self.pickup_pub = rospy.Publisher('pickup_done', Bool, queue_size=10)
+
+        self.picking_can = False
+
+        # Run Calibration of Joints
+        self.calibrate()
+
+    def calibrate(self):
+        """
+        Calibrates all servos
+        """
+        # Open the Gripper
+        self.gripper(True)
+        # Move the joint to over the bucket
+        self.elbow(2000)
+
+    def gripper(self, val, accel=4, speed=10):
+        """
+        :param val: True opens the gripper, False closes the gripper
+        :param accel: acceleration of servo
+        :param speed: speed of servo movement
+        :return: void
+        """
+        self.servo.setAccel(self.gripper_pin, accel)  # set gripper acceleration
+        self.servo.setSpeed(self.gripper_pin, speed)  # set gripper speed
+
+        if val:
+            self.servo.setTarget(self.gripper_pin, 1000)  # set gripper position
+        else:
+            self.servo.setTarget(self.gripper_pin, 1500)  # set gripper position
+
+    def get_gripper_pos(self):
+        """
+        Getter for gripper position
+        :return: Position of the gripper
+        """
+        return self.servo.getPosition(self.gripper_pin)  # get the current position of gripper servo
+
+    def elbow(self, val, accel=4, speed=10):
+        """
+        :param val: True opens the gripper, False closes the gripper
+        :param accel: acceleration of servo
+        :param speed: speed of servo movement
+        :return: void
+        """
+        self.servo.setAccel(self.elbow_pin, accel)  # set gripper acceleration
+        self.servo.setSpeed(self.elbow_pin, speed)  # set gripper speed
+        self.servo.setTarget(self.elbow_pin, val)  # set gripper position
+
+    def get_elbow_pos(self):
+        """
+        Getter for gripper position
+        :return: Position of the elbow
+        """
+        return self.servo.getPosition(self.elbow_pin)  # get the current position of elbow servo
+
+    def pickup_can(self):
+        """
+        Pickup the Can
+        """
+        self.picking_can = True
+
+        while self.picking_can:
+            print("Picking Up Can")
+            # Open Gripper
+            self.gripper(True)
+            # Move Elbow down
+            self.elbow(1000)
+            rospy.sleep(4)
+
+            # Close Gripper
+            self.gripper(False)
+            rospy.sleep(3)
+
+            # Move Arm back up
+            self.elbow(1000)
+            rospy.sleep(3)
+
+            # Open the Gripper
+            self.gripper(True)
+            rospy.sleep(3)
+
+            # Done Picking up
+            self.picking_can = False
+            rospy.sleep(5)
+
+        bool_msg = Bool()
+        bool_msg.data = True
+        self.pickup_pub.publish(bool_msg)
+
+    def cleanup(self):
+        """
+        Cleans up if keyboard interrupt
+        """
+        self.servo.close()
+
+
+if __name__ == "__main__":
+    sc = ServoController()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        sc.cleanup()
--- test/src/test/servo_controller_test.py
+++ test/src/test/servo_controller_test.py
@@ -6,7 +6,7 @@ class ServoController:
     def __init__(self):
         servo = maestro.Controller()
         servo.setAccel(0, 4)  # set servo 0 acceleration to 4
-        servo.setSpeed(1, 10)  # set speed of servo 1
+        servo.setSpeed(0, 10)  # set speed of servo
         servo.setTarget(0, 6000)  # set servo to move to center position
         # x = servo.getPosition(1)  # get the current position of servo 1
         servo.close()
