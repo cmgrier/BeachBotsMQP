2a4b3ffd53f730d31e4e055a7a725d58afe9a835
Everett Johnson <everettreecejohnson564@gmail.com>
2020-04-23 17:38:14 -0400

    Fixed Constants after rework

--- test/src/test/servo_controller_test.py
+++ test/src/test/servo_controller_test.py
@@ -7,7 +7,7 @@ class ServoController:
         servo = maestro.Controller('/dev/ttyAMA0')
         servo.setAccel(0, 4)  # set servo 0 acceleration to 4
         servo.setSpeed(0, 10)  # set speed of servo
-        servo.setTarget(0, 6000)  # set servo to move to center position
+        servo.setTarget(0, 2000)  # set servo to move to center position
         # x = servo.getPosition(1)  # get the current position of servo 1
         servo.close()
 
