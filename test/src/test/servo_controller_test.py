6154d2041b04e97c861f12ff49566c07cbd1edd5
Everett Johnson <everettreecejohnson564@gmail.com>
2020-04-23 21:38:04 -0400

    Fixed Constants after rework

--- test/src/test/servo_controller_test.py
+++ test/src/test/servo_controller_test.py
@@ -15,9 +15,9 @@ class ServoController:
         # servo.setRange(1, 0, 12000)
         # print(servo.getMax(1))
         # print(servo.getMin(1))
-        servo.setTarget(1, 10000)  # set servo to move to center position
+        servo.setTarget(1, 7000)  # set servo to move to center position
         time.sleep(5)
-        servo.setTarget(1, 1000)
+        servo.setTarget(1, 3000)
         time.sleep(5)
         # x = servo.getPosition(1)  # get the current position of servo 1
         print("Done")
